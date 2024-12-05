021a9d80bdc51abb1ecdd40dd1d2deb50e7fdffb
JensenOcampo <jocampo_21ur0254@psu.edu.ph>
2024-12-05 22:46:49 +0800

    upload

--- dashboard.php
+++ dashboard.php
@@ -4,12 +4,15 @@ include 'config.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
 </head>
+
 <body>
     <h1>WELCOME</h1>
 </body>
+
 </html>
